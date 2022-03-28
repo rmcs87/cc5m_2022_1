b1a94f337b82423a5c2620535ea2721da9805054
Ricardo Mendes <rmcs87@gmail.com>
2022-03-28 11:18:07 +0000

    Aula 11 - Estrutura Básica

---
+++ snipetbox/go.mod
@@ -0,0 +1,3 @@
+module github.com/rmcs87/cc5m
+
+go 1.16
---
+++ snipetbox/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(rw http.ResponseWriter, r *http.Request){
+  rw.Write([]byte("Bem vindo ao SnipetBox"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+  mux.HandleFunc("/", home)
+  log.Println("Inicializando o servidor na porta: 4000")
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
+}
