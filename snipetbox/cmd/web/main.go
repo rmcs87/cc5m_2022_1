a4d5521b65c7649af2f0bd67ffa289e2fd4d9d0d
Ricardo Mendes <rmcs87@gmail.com>
2022-04-18 11:39:46 +0000

    Fim aula  15 - loggers, flags e DI

--- snipetbox/cmd/web/main.go
+++ snipetbox/cmd/web/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct{
+  errorLog *log.Logger
+  infoLog *log.Logger
+}
 
 //curl -i -X GET http://localhost:4000/snippet/create
 func main() {
-	mux := http.NewServeMux()
-  
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet", showSnippet)
-  mux.HandleFunc("/snippet/create", createSnippet)
+	// nome da flag, valor padra e descrição
+	addr := flag.String("addr", ":4000", "Porta da Rede")
+	flag.Parse()
 
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
-  mux.Handle("/static/",http.StripPrefix("/static",fileServer))
-  
-  log.Println("Inicializando o servidor na porta: 4000")
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERRO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+  app := &application{
+    errorLog: errorLog,
+    infoLog: infoLog,
+  }
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	infoLog.Printf("Inicializando o servidor na porta %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
