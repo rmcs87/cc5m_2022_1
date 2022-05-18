dc9c29aa195eb0fa3793a78c1bab9c197a33c6c5
Ricardo Mendes <rmcs87@gmail.com>
2022-05-18 13:25:37 +0000

    Middleware

--- snipetbox/cmd/web/routes.go
+++ snipetbox/cmd/web/routes.go
@@ -2,7 +2,8 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
+  
   mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -10,5 +11,6 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  return mux
+  return app.logRequest( secureHeaders( mux ) )
 }
+//https://cc5m.ricardomendes2.repl.co
